a2c663c7eaa7db67695fd0c226f741c7e8f54b41
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2022-08-26 01:55:44 +0200

    Time: 21 ms (12.48%), Space: 13.7 MB (63.54%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
+    int BSFirst(vector<int>& nums, int target){
+        int l=0,r=nums.size()-1,pos=-1,mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(nums[mid]>target){
+                r=mid-1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                pos=mid;
+                r=mid-1;
+            }
+        }
+        return pos;
+    }
+      int BSLast(vector<int>& nums, int target){
+        int l=0,r=nums.size()-1,pos=-1,mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(nums[mid]>target){
+                r=mid-1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                pos=mid;
+                l=mid+1;
+            }
+        }
+        return pos;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-          int s=0,e=0;
-        map<int,int>m;
-        for(int i=0;i<nums.size();i++)m[nums[i]]++;
-         if(!m[target])return vector<int>{-1,-1};
-        s=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        e=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        return vector<int>{s,e-1};
+          int l=BSFirst(nums,target),r=BSLast(nums,target);
+        if(l==-1||r==-1)return vector<int>{-1,-1};
+        else return vector<int>{l,r};
     }
 };

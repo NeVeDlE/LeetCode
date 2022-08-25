27a43d5e1ae0e33aa73f455a62c788fcdc949515
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2022-08-26 01:59:54 +0200

    Time: 16 ms (36.32%), Space: 13.7 MB (63.54%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,12 @@
 class Solution {
 public:
-    int BSFirst(vector<int>& nums, int target){
-        int l=0,r=nums.size()-1,pos=-1,mid;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            if(nums[mid]>target){
-                r=mid-1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                pos=mid;
-                r=mid-1;
-            }
-        }
-        return pos;
-    }
-      int BSLast(vector<int>& nums, int target){
-        int l=0,r=nums.size()-1,pos=-1,mid;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            if(nums[mid]>target){
-                r=mid-1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                pos=mid;
-                l=mid+1;
-            }
-        }
-        return pos;
-    }
+ 
     vector<int> searchRange(vector<int>& nums, int target) {
-          int l=BSFirst(nums,target),r=BSLast(nums,target);
-        if(l==-1||r==-1)return vector<int>{-1,-1};
-        else return vector<int>{l,r};
+         auto p=equal_range(nums.begin(),nums.end(),target);
+        if(p.first==nums.end())return {-1,-1};
+        int x=p.first-nums.begin();
+        int y=p.second-nums.begin();
+        if(nums[x]!=target)return {-1,-1};
+        return {x,y-1};
     }
 };

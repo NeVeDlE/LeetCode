3979c82ff2c87bae7d3ac0503988d26a5d7b9b6a
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2022-08-26 07:45:52 +0200

    Time: 172 ms (19.67%), Space: 30.4 MB (21.59%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    vector<int> findRightInterval(vector<vector<int>>& intervals) {
-      vector<pair<int,int> > v;
-        map<pair<int,int> ,int> m;
-        for(int i=0;i<intervals.size();i++){
-            v.push_back({intervals[i][0],intervals[i][1]});
-            m[v[i]]=i;
-        }
-        sort(v.begin(),v.end());
-        vector<int>ans;
-        for(int i=0;i<intervals.size();i++){
-            int l=0,r=v.size()-1,mid;
+    int BS(vector<vector<int> > &v,int target){
+           int l=0,r=v.size()-1,mid;
             int tmp=-1;
             while(l<=r){
                 mid=l+(r-l)/2;
-                if(v[mid].first>=intervals[i][1]){
+                if(v[mid][0]>=target){
                     tmp=mid;
                     r=mid-1;
                 }
@@ -22,9 +13,18 @@ public:
                     l=mid+1;
                 }
             }
-            if(tmp!=-1)
-            ans.push_back(m[v[tmp]]);
-            else ans.push_back(-1);
+        if(tmp==-1)return -1;
+        return v[tmp][1];
+    }
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+      vector<vector<int> > v;
+        for(int i=0;i<intervals.size();i++){
+            v.push_back({intervals[i][0],i});
+        }
+        sort(v.begin(),v.end());
+        vector<int> ans;
+        for(int i=0;i<intervals.size();i++){
+            ans.push_back(BS(v,intervals[i][1]));
         }
         return ans;
     }

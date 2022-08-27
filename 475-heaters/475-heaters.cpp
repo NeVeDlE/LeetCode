e2e034e769b06920026a5792c830b58ce03560d8
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2022-08-27 10:25:15 +0200

    Time: 103 ms (58.86%), Space: 25.3 MB (58.20%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int BSLeft(int house, vector<int>& heaters){
+        int l=0,r=heaters.size()-1,mid;
+        int ans=-1;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(heaters[mid]<=house){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+     int BSRight(int house, vector<int>& heaters){
+        int l=0,r=heaters.size()-1,mid;
+        int ans=-1;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(heaters[mid]>=house){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+    
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+              int ans=0;
+        for(int i=0;i<houses.size();i++){
+           int left=BSLeft(houses[i],heaters);
+            int right=BSRight(houses[i],heaters);
+          if(left==-1){
+              ans=max(heaters[right]-houses[i] ,ans);
+          }
+            else if(right==-1){
+                    ans=max(houses[i]-heaters[left] ,ans);
+            }
+            else{
+              ans=max(min(heaters[right]-houses[i],houses[i]-heaters[left]),ans);
+            }
+
+        }
+        return ans;
+    }
+};

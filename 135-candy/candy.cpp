b988f674cea877badaf5535c0a60ad0a221f75e4
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2024-01-21 05:25:55 +0200

    Time: 14 ms (82.44%) | Memory: 20.4 MB (5.19%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,7 +3,6 @@ public:
     int candy(vector<int>& r) {
         vector<int>pre(r.size(),1);
          vector<int>suf(r.size(),1);
-         cout<<pre.size()<<" "<<r.size()<<endl;
         for(int i=0;i<r.size();i++){
             if (i+1<r.size()&&r[i+1]<r[i]&&i>0&&r[i-1]<r[i]){
                  pre[i]=max(pre[i+1],pre[i-1])+1;
@@ -17,7 +16,6 @@ public:
         }
         for(int i=r.size()-1;i>=0;i--){
             if(i>0&&r[i-1]<r[i]&&i+1<r.size()&&r[i+1]<r[i]){
-                 cout<<r[i]<<endl;
                  suf[i]=max(suf[i-1],suf[i+1])+1;
              }
            else   if(i>0&&r[i-1]<r[i]){

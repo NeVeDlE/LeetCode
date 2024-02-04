b1441f1effd65f2025eea6dcc067348126d4c433
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2024-02-04 20:11:22 +0200

    Time: 9 ms (29.34%) | Memory: 9.3 MB (20.38%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,7 +4,6 @@ public:
         map<char,char>m;  
         map<char,int>n;
         for(int i=0;i<s.size();i++){
-            cout<<s[i]<<" "<<t[i]<<endl;
          if(!m[s[i]]){
              if(n[t[i]])return false;
              n[t[i]]++;

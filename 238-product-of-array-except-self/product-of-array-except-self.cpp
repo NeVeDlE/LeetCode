6d78719cd6dc3f858007edac9321c49662631191
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2024-11-10 17:03:56 +0200

    Time: 0 ms (100.00%) | Memory: 38.3 MB (93.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int ans = 1;
-        bool tried = 0;
-        bool z = 0;
-        bool zz = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (!nums[i] && !z)
-                z = 1;
-            else if (!nums[i] && z)
-                zz = 1;
-            else {
-                ans *= nums[i];
-                tried = 1;
-            }
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int product = 1;
+    int zeroCount = 0;
+    for (int num : nums) {
+        if (num == 0) {
+            zeroCount++;
+        } else {
+            product *= num;
         }
-
-        if (zz) {
-            return vector<int>(nums.size(), 0);
-        }
-        if (!tried)
-            return vector<int>(nums.size(), 0);
-        vector<int> tmp;
-        if (z) {
-
-            for (int i = 0; i < nums.size(); i++) {
-                if (!nums[i])
-                    tmp.push_back(ans);
-                else
-                    tmp.push_back(0);
-            }
-            return tmp;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            tmp.push_back(ans / nums[i]);
-        }
-
-        return tmp;
     }
+    vector<int> result(nums.size(), 0);
+    if (zeroCount > 1) {
+        return result;
+    }
+    if (zeroCount == 1) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                result[i] = product;
+            }
+        }
+        return result;
+    }
+    for (int i = 0; i < nums.size(); i++) {
+        result[i] = product / nums[i];
+    }
+    return result;
+}
+
 };

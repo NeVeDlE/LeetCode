b83798bc36dc4ba159166c2c4520055e0d035524
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2025-09-15 16:10:17 +0300

    Time: 3 ms (18.94%) | Memory: 8.9 MB (69.10%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        map<string, int> m;
         string s = "";
-        for (int i = 0; i < text.size(); i++) {
-            if (text[i] == ' ' || i == text.size() - 1) {
-                if (i == text.size() - 1)
-                    s += text[i];
-                m[s]++;
-                s = "";
-            } else {
-                s += text[i];
-            }
-        }
+        bool valid = 0;
         int ans = 0;
-
-        for (auto it : m) {
-            bool f = 0;
-            cout << it.first << " " << endl;
-            for (int i = 0; i < it.first.size(); i++) {
-                for (int j = 0; j < brokenLetters.size(); j++) {
-                    if (it.first[i] == brokenLetters[j]) {
-                        f = 1;
-                        break;
-                    }
+        for (int i = 0; i < text.size(); i++) {
+            if (text[i] == ' ') {
+                if (!valid)
+                    ans++;
+                valid = 0;
+            }
+            for (int j = 0; j < brokenLetters.size(); j++) {
+                if (text[i] == brokenLetters[j]) {
+                    valid = 1;
+                    break;
                 }
             }
-            if (!f)
-                ans+=it.second;
         }
+        if(!valid)ans++;
+
         return ans;
     }
 };

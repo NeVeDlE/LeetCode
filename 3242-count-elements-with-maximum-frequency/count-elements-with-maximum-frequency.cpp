e246e69406a03beea54cd27a9a32579982491672
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2025-09-22 15:01:22 +0300

    Time: 0 ms (100.00%) | Memory: 22.7 MB (99.92%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    int arr[101];
     int maxFrequencyElements(vector<int>& nums) {
-        map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
-        }
-        int mx=0;
-        for(auto i:m){
-         mx=max(i.second,mx);
-        }
-        int ans=0;
-        for(auto i:m)if(i.second==mx)ans+=i.second;
-        return ans;
+        int mx = 0;
+        int mxN = 0;
+        for (int i = 0; i < nums.size(); i++)
+            mx = max(mx, ++arr[nums[i]]);
+        for (int i = 0; i < nums.size(); i++)
+            if (arr[nums[i]] == mx){
+                mxN += mx;
+                arr[nums[i]]=0;
+            }
+
+        return mxN;
     }
 };
